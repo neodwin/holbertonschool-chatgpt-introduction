fd1e4f5fc3126c6a325175be2a696c93d2735f21
neodwin <neo60x@hotmail.com>
2025-01-13 10:05:34 +0100

    Add factorial.py file

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 import sys
 
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid infinite loop
+        n -= 1  # Réduction de n pour éviter une boucle infinie
     return result
 
 f = factorial(int(sys.argv[1]))
